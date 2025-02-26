e9a93994dba9f6ef085db315b72ed99718e30c3f
DavidAngel07 <davidangelosorio29@gmail.com>
2025-02-25 21:10:54 -0500

    Cambio de bases de datos

--- backend/backend/server.js
+++ backend/backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: process.env.MYSQL_HOST || 'mysql.railway.internal',
   user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'xIbwUShObLhAtbkPlQaeeTWszVKVOmkj',
+  password: process.env.MYSQL_PASSWORD || 'PRyQrtojaQmbSELgrRirBkSFHMKGSQrq',
   database: process.env.MYSQL_DATABASE || 'railway',
   port: process.env.MYSQL_PORT || '3306',
 });
